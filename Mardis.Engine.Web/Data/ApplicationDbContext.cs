cb2876954b4e7c95263ae9fc993c526957618f6b
Fabian Aguirre <fabian10119@hotmail.com>
2017-11-24 15:36:36 -0500

    Subir proyectos Emgine

---
+++ Mardis.Engine.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Mardis.Engine.Web.Libraries.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Mardis.Engine.Web.Model;
+
+namespace Mardis.Engine.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+       
+    }
+}
